28e56d5ae02fab1e0de77a55fee101eeaf2f5d76
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2023-06-15 15:35:49 -0400

    made-the-search-bar-work

--- src/functions.js
+++ src/functions.js
@@ -1,2 +1,17 @@
-import { getCurrent, getForecast } from "./api";
+import { keyCurrent,keyForecast } from "./api";
 import{ yourIpForecast,autoIp } from "./api";
+let searchButton = document.querySelector('.submit');
+let searchInput = document.getElementById('search');
+let container = document.querySelector('.container');
+let todayLeft=document.querySelector('.todayLeft');
+let todayRight=document.querySelector('.todayRight');
+let todayCont = document.querySelector('.todayCont');
+export function search(){searchButton.addEventListener('click', function(event) {
+    event.preventDefault();
+   container.innerText="";
+   todayCont.innerText="";
+  const keyword = searchInput.value;
+  searchInput.value="";
+keyCurrent(keyword);
+keyForecast(keyword);
+});}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { getForecast,getCurrent } from "./api";
 import "./styles.css";
-// import { search } from "./functions";
+import { search } from "./functions";
 getCurrent()
 getForecast()
-// search()
+search()
 
